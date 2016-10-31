0a806be2e5373e10072dab1c17fc26de2986ddc9
elyl <elyl@debian>
2016-10-31 16:12:30 +0100

    toto

--- project/src/main/java/tiir/ifi/project/App.java
+++ project/src/main/java/tiir/ifi/project/App.java
@@ -6,8 +6,8 @@ package tiir.ifi.project;
  */
 public class App 
 {
-    public static void main( String[] args )
+    /*    public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-    }
+	}*/
 }
