c7b7791efe18de84fcd719c24e95e69ad24d5b2f
elyl <elyl@debian>
2016-11-01 09:57:53 +0100

    L'exemple marche mais en standalone pas sur tomcat. J2EE c'est du caca

--- project/src/main/java/tiir/ifi/project/Application.java
+++ project/src/main/java/tiir/ifi/project/Application.java
@@ -10,10 +10,10 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer{
 
-    @Override
+        @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 	return application.sources(Application.class);
-    }
+	}
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
